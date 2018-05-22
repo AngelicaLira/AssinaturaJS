6225f28e8206f9bb5ef4588e47e0dd182ffdc95b
Angelica Lira <angelicavfreitas@gmail.com>
2018-05-22 17:25:13 -0300

    ass

---
+++ index.js
@@ -0,0 +1,6 @@
+const moip = require('moip-sdk-node').default({
+    accessToken: '8e29bc8f46c342f3952ac4b96e80b76c_v2',
+    // token: 'your-token',
+    // key: 'your-key',
+    production: false
+  })
